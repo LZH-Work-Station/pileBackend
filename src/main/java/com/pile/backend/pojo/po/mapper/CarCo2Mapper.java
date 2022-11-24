be4b9d290ce130db6faa4170ee39b33aa0ab7de3
lizehan <zehan_li@163.com>
2022-11-24 19:03:52 +0800

    Add data in mysql

---
+++ src/main/java/com/pile/backend/pojo/po/mapper/CarCo2Mapper.java
@@ -0,0 +1,11 @@
+package com.pile.backend.pojo.po.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.pile.backend.pojo.po.CarCo2;
+import com.pile.backend.pojo.po.Gare;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface CarCo2Mapper extends BaseMapper<CarCo2> {
+}
